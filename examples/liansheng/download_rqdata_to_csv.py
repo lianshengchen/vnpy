1e8ad88ca54f219de864e5ba79b2dd4d23428c11
Cloud User <centos@ip-172-31-10-156.us-west-1.compute.internal>
2020-02-19 09:01:25 +0000

    tmp

--- examples/liansheng/download_rqdata_to_csv.py
+++ examples/liansheng/download_rqdata_to_csv.py
@@ -5,7 +5,11 @@ import pandas as pd
 rq.init()
 
 
-rq_result = rq.get_price('000001.XSHE','2019-1-1','2019-2-01','tick')
+#rq_result = rq.get_price('000001.XSHE','2018-1-1','2018-12-31','tick')
+rq_result = get_price('000001.XSHE',start_date='2019-1-1',end_date='2019-12-31',frequency=interval,
+                fields=["open", "high", "low", "close", "volume", "open_interest"],
+                adjust_type="none"
+                )
 df = pd.DataFrame(rq_result)
-df.to_csv('000001.XSHE.csv')
+df.to_csv('000001.XSHE.2019.bar1m.csv')
 
